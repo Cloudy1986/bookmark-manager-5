12541bd067676dc9f3d5dbdba95d0b7ceee5e319
Adam Harbisher <aharbisher@gmail.com>
2021-11-11 14:39:39 +0000

    Updated feature test to expect bookmark title

--- spec/features/creating_bookmarks_spec.rb
+++ spec/features/creating_bookmarks_spec.rb
@@ -1,9 +1,11 @@
 feature 'Adding a new bookmark' do
   scenario 'A user can add a bookmark to Bookmark Manager' do
     visit('bookmarks/new')
-    fill_in('url', with: 'http://example.org')
+    fill_in('url', with: 'http://bbc.co.uk')
+    fill_in('title', with: 'BBC')
     click_button('Submit')
-
-    expect(page).to have_content 'http://example.org'
+    expect(page).to have_link('BBC')
+    click_link('BBC')
+    expect(page).to have_content('Welcome to the BBC')
   end
 end
